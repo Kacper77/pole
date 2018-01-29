9871dbdcbacc75f124e9f0a56dfeeb940a98c69e
Kacper Stroisz <kacperstroisz@wp.pl>
2018-01-29 15:02:23 +0100

    Change files

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+
+var a = 10;
+var h = 5;
+var triangleArea = a*h/2;
+
+	
+alert('a*h/2 to: ' + triangleArea);
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
